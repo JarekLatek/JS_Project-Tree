6acff7f81734150add41dc2859e4f07cadd2bc9d
Jarek Latek <jareklatek@gmail.com>
2017-06-05 16:09:12 +0200

    Add symetric to the tree in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ function drawTree(h) {
 	
 	for ( var i = 0; i < h; i++ ) {
 		var star = '';
-		for ( var j = 0; j <= i; j++) {
+		for ( var s = 0; s < h - i; s++ ) {
+			star += ' ';
+		}
+		for ( var j = 0; j < ( 2 * i + 1); j++) {
 			star += '*';
 		}
 		console.log(star);
